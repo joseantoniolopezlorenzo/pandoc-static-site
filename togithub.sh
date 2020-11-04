b966f62b422a1c322c3a713e07e21ea8bef284e5
joseantoniolopezlorenzo <jall.informatico@gmail.com>
2020-11-04 10:03:15 +0100

    var1

--- togithub.sh
+++ togithub.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-if [ -z "$1" ]
-then
-  echo "Mensaje para el commit"
-  exit 1
-fi
+echo 'Introduzca el mensaje para el commit:'
+#leer el dato del teclado y guardarlo en la variable de usuario var1
+read var1
 npm run build
-git add . && git commit -m $1 && git push
+git add . && git commit -m 'var1' #&& git push
+#git subtree push --prefix dist origin gh-pages
